c50f096db7ebc0361cd71f94c1aa552f520498eb
Mark Luetke <luetkemj@gmail.com>
2016-06-13 07:19:52 -0500

    fix reducer.test
    Test worked locally due to uncommitted WIP but broke in CI qhwn
    isolated to just the mock_api branch. Stashed WIP and fixed test for
    what we are actually submitting with this branch.

--- test/app/reducers/week.reducer.test.js
+++ test/app/reducers/week.reducer.test.js
@@ -6,12 +6,54 @@ describe('week reducer', () => {
     should.exist(reducer);
   });
 
-  it('should have the correct initial state', () => {
+  const weeks = [
+    {
+      name: 'Monday',
+      goal: 6,
+      actual: 0,
+      variance: -6,
+    },
+    {
+      name: 'Tuesday',
+      goal: 6,
+      actual: 5,
+      variance: -1,
+    },
+    {
+      name: 'Wednesday',
+      goal: 6,
+      actual: 6.1,
+      variance: 0.1,
+    },
+    {
+      name: 'Thursday',
+      goal: 6,
+      actual: 5.9,
+      variance: -0.1,
+    },
+    {
+      name: 'Friday',
+      goal: 6,
+      actual: 7.1,
+      variance: 1.1,
+    },
+    {
+      name: 'Saturday',
+      goal: 6,
+      actual: 6,
+      variance: 0,
+    },
+    {
+      name: 'Sunday',
+      goal: 6,
+      actual: 7,
+      variance: 1,
+    },
+  ];
+
+  it('should have to correct initial state', () => {
     should(reducer(undefined, {})).deepEqual({
-      weeks: [],
-      loadingWeek: false,
-      editingWeek: false,
-      savingWeek: false,
+      weeks,
     });
   });
 });
